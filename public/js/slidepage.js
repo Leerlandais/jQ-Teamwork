5742a16041779f422a9125090ab47b3724dc8aa1
leerlandais <leerlandais@gmail.com>
2024-06-01 19:29:29 +0200

    ready for tomorrow

--- public/js/slidepage.js
+++ public/js/slidepage.js
@@ -1,9 +1,1 @@
-
-/*
-Si tu veux un challenge pour demain, j'en ai un grand pour toi.....
-
-J'ai mis tout notre projet sur Git afin qu'on puisse travailler manière correct.
-Je te previens qu'il n'y a pas mal d'étapes mais je vais les décrire (dessous). En tout cas, si tu n'arrives pas faire via Git, tu peux continuer d'envoyer en .zip
-Mais ce serait une bonne exercice!
-
-Alors, il faut que tu ailles vers mon Git
+// tomorrow is another day ;)
