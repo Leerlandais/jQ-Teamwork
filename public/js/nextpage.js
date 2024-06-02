e9a2d19c89e190ba78f679471931a2d472eb1243
leerlandais <leerlandais@gmail.com>
2024-06-02 11:24:33 +0200

    this one

--- public/js/nextpage.js
+++ public/js/nextpage.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
     $('#activateCodeButtonC0').click(function(){
         console.log('clicked'); // pour verifier que le clic et entendu
-    
+        $("codeDescSpanC0")  // ceci est l'element sur lequel tu fait l'effet désiré
         // ton code ici
 
     });
